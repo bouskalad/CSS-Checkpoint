794c92fc1a03c4ceaec32f5a959c01592bc9a10e
bouskalad <bouskabenz@gmail.com>
2024-03-27 13:28:06 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const container = document.querySelector(".container");
+let btn = document.querySelector(".btn");
+const form = document.querySelector(".form");
+
+
+
+btn.addEventListener("click", () =>{
+    form.style.visibility = "visible";
+});
